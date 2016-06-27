41533e106dc0bee441126da99181a38ced1d5c72
sofia <sofia.yu@voyageone.cn>
2016-06-27 18:26:39 +0800

    primary version

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var WebpackDevServer = require("webpack-dev-server");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    "vendor": "./app/vendor",
+    "app": "./app/boot"
+  },
+  output: {
+    path: __dirname,
+    filename: "./dist/[name].bundle.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.ts/,
+        loaders: ['ts-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./dist/vendor.bundle.js")
+  ]
+}
